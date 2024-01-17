c60585243be3701692c6f6f6b6bb14ba74e7ae4c
Apollo <102649729+Apollounknowndev@users.noreply.github.com>
2024-01-16 19:37:10 -0800

    Fix some issues(?)

--- neoforge/src/main/java/dev/worldgen/lithostitched/LithostitchedNeoforge.java
+++ neoforge/src/main/java/dev/worldgen/lithostitched/LithostitchedNeoforge.java
@@ -3,7 +3,6 @@ package dev.worldgen.lithostitched;
 import dev.worldgen.lithostitched.registry.LithostitchedBuiltInRegistries;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 
 /**
  * Mod class for Lithostitched on Forge.
@@ -11,9 +10,8 @@ import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(LithostitchedCommon.MOD_ID)
 public final class LithostitchedNeoforge {
 
-	public LithostitchedNeoforge() {
+	public LithostitchedNeoforge(IEventBus bus) {
 		LithostitchedCommon.init();
-		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 		LithostitchedBuiltInRegistries.init(bus);
 	}
 
