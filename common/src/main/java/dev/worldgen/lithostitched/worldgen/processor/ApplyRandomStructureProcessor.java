ce05280223b31a50828e79be7097ffb0e62041d2
Apollo <102649729+Apollounknowndev@users.noreply.github.com>
2024-01-19 13:35:33 -0800

    1.1.4

--- common/src/main/java/dev/worldgen/lithostitched/worldgen/processor/ApplyRandomStructureProcessor.java
+++ common/src/main/java/dev/worldgen/lithostitched/worldgen/processor/ApplyRandomStructureProcessor.java
@@ -8,30 +8,30 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.HolderSet;
 import net.minecraft.core.RegistryCodecs;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.util.random.SimpleWeightedRandomList;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.levelgen.structure.templatesystem.*;
 import org.jetbrains.annotations.NotNull;
 
 public class ApplyRandomStructureProcessor extends StructureProcessor {
     public static final Codec<ApplyRandomStructureProcessor> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-        RegistryCodecs.homogeneousList(Registries.PROCESSOR_LIST, StructureProcessorType.DIRECT_CODEC).fieldOf("processor_lists").forGetter(ApplyRandomStructureProcessor::processorLists)
+            SimpleWeightedRandomList.wrappedCodec(StructureProcessorType.LIST_CODEC).fieldOf("processor_lists").forGetter(ApplyRandomStructureProcessor::processorLists)
     ).apply(instance, ApplyRandomStructureProcessor::new));
 
     public static final StructureProcessorType<ApplyRandomStructureProcessor> APPLY_RANDOM_TYPE = () -> CODEC;
-    private final HolderSet<StructureProcessorList> processorLists;
+    private final SimpleWeightedRandomList<Holder<StructureProcessorList>> processorLists;
 
-    public ApplyRandomStructureProcessor(HolderSet<StructureProcessorList> processorLists) {
+    public ApplyRandomStructureProcessor(SimpleWeightedRandomList<Holder<StructureProcessorList>> processorLists) {
         this.processorLists = processorLists;
     }
 
-    public HolderSet<StructureProcessorList> processorLists() {
+    public SimpleWeightedRandomList<Holder<StructureProcessorList>> processorLists() {
         return this.processorLists;
     }
 
     @Override
     public StructureTemplate.StructureBlockInfo processBlock(LevelReader levelReader, BlockPos blockPos, BlockPos blockPos2, StructureTemplate.StructureBlockInfo structureBlockInfo, StructureTemplate.StructureBlockInfo currentBlockInfo, StructurePlaceSettings structurePlaceSettings) {
-        LithostitchedCommon.LOGGER.info(String.valueOf(processorLists.size()));
-        Holder<StructureProcessorList> processorList = processorLists.get(structurePlaceSettings.getRandom(currentBlockInfo.pos()).nextInt(processorLists.size()));
+        Holder<StructureProcessorList> processorList = processorLists.getRandomValue(structurePlaceSettings.getRandom(currentBlockInfo.pos())).get();
         for (StructureProcessor processor : processorList.value().list()) {
             StructureTemplate.StructureBlockInfo candidateBlockInfo = processor.processBlock(levelReader, blockPos, blockPos2, structureBlockInfo, currentBlockInfo, structurePlaceSettings);
             if (candidateBlockInfo != currentBlockInfo) {
