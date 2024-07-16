4e280dd32c820c0b69d42ec9c78cad7defb6e4e3
Apollo <102649729+Apollounknowndev@users.noreply.github.com>
2024-07-16 14:22:58 -0400

    v1.2.3

--- common/src/main/java/dev/worldgen/lithostitched/worldgen/processor/ApplyRandomStructureProcessor.java
+++ common/src/main/java/dev/worldgen/lithostitched/worldgen/processor/ApplyRandomStructureProcessor.java
@@ -3,12 +3,9 @@ package dev.worldgen.lithostitched.worldgen.processor;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import dev.worldgen.lithostitched.LithostitchedCommon;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
-import net.minecraft.core.HolderSet;
-import net.minecraft.core.RegistryCodecs;
-import net.minecraft.core.registries.Registries;
+import net.minecraft.util.StringRepresentable;
 import net.minecraft.util.random.SimpleWeightedRandomList;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.levelgen.structure.templatesystem.*;
@@ -16,23 +13,31 @@ import org.jetbrains.annotations.NotNull;
 
 public class ApplyRandomStructureProcessor extends StructureProcessor {
     public static final MapCodec<ApplyRandomStructureProcessor> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
-            SimpleWeightedRandomList.wrappedCodec(StructureProcessorType.LIST_CODEC).fieldOf("processor_lists").forGetter(ApplyRandomStructureProcessor::processorLists)
+        SimpleWeightedRandomList.wrappedCodec(StructureProcessorType.LIST_CODEC).fieldOf("processor_lists").forGetter(ApplyRandomStructureProcessor::processorLists),
+        Mode.CODEC.fieldOf("mode").forGetter(ApplyRandomStructureProcessor::mode)
     ).apply(instance, ApplyRandomStructureProcessor::new));
 
     public static final StructureProcessorType<ApplyRandomStructureProcessor> APPLY_RANDOM_TYPE = () -> CODEC;
     private final SimpleWeightedRandomList<Holder<StructureProcessorList>> processorLists;
+    private final Mode mode;
 
-    public ApplyRandomStructureProcessor(SimpleWeightedRandomList<Holder<StructureProcessorList>> processorLists) {
+    public ApplyRandomStructureProcessor(SimpleWeightedRandomList<Holder<StructureProcessorList>> processorLists, Mode mode) {
         this.processorLists = processorLists;
+        this.mode = mode;
     }
 
     public SimpleWeightedRandomList<Holder<StructureProcessorList>> processorLists() {
         return this.processorLists;
     }
 
+    public Mode mode() {
+        return this.mode;
+    }
+
     @Override
     public StructureTemplate.StructureBlockInfo processBlock(LevelReader levelReader, BlockPos blockPos, BlockPos blockPos2, StructureTemplate.StructureBlockInfo structureBlockInfo, StructureTemplate.StructureBlockInfo currentBlockInfo, StructurePlaceSettings structurePlaceSettings) {
-        Holder<StructureProcessorList> processorList = processorLists.getRandomValue(structurePlaceSettings.getRandom(currentBlockInfo.pos())).get();
+        BlockPos randomPos = this.mode == Mode.PER_BLOCK ? currentBlockInfo.pos() : blockPos;
+        Holder<StructureProcessorList> processorList = processorLists.getRandomValue(structurePlaceSettings.getRandom(randomPos)).get();
         for (StructureProcessor processor : processorList.value().list()) {
             StructureTemplate.StructureBlockInfo candidateBlockInfo = processor.processBlock(levelReader, blockPos, blockPos2, structureBlockInfo, currentBlockInfo, structurePlaceSettings);
             if (candidateBlockInfo != currentBlockInfo) {
@@ -45,5 +50,22 @@ public class ApplyRandomStructureProcessor extends StructureProcessor {
     @Override
     protected @NotNull StructureProcessorType<?> getType() {
         return APPLY_RANDOM_TYPE;
+    }
+
+    public enum Mode implements StringRepresentable {
+        PER_BLOCK("per_block"),
+        PER_PIECE("per_piece");
+
+        public static final Codec<Mode> CODEC = StringRepresentable.fromEnum(Mode::values);
+        private final String name;
+
+        Mode(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String getSerializedName() {
+            return this.name;
+        }
     }
 }
