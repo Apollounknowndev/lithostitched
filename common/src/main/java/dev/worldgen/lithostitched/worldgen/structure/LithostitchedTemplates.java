63d52b6e2c76da6e32ca1a859b8ff99034420dc6
Apollo <102649729+Apollounknowndev@users.noreply.github.com>
2024-11-22 00:47:50 -0500

    Jigsaw structure stuff

--- common/src/main/java/dev/worldgen/lithostitched/worldgen/structure/LithostitchedTemplates.java
+++ common/src/main/java/dev/worldgen/lithostitched/worldgen/structure/LithostitchedTemplates.java
@@ -25,17 +25,15 @@ public class LithostitchedTemplates implements Iterable<StructurePoolElement> {
     }
 
     public LithostitchedTemplates shuffle(RandomSource random, int depth) {
+        this.entries.sort(Comparator.comparingInt(WeightedEntry::getIndex));
+
         this.entries.forEach(entry -> entry.setRandom(random.nextFloat(), depth));
         this.entries.sort(Comparator.comparingDouble(WeightedEntry::getRandWeight));
         return this;
     }
 
     public Stream<StructurePoolElement> stream() {
-        Stream<StructurePoolElement> stream = this.entries.stream().map(WeightedEntry::getData);
-
-        this.entries.sort(Comparator.comparingInt(WeightedEntry::getIndex));
-
-        return stream;
+        return this.entries.stream().map(WeightedEntry::getData);
     }
 
     @Override
