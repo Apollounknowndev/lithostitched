cdff5efbc24aae79a2d3ded64e9ec4373ba248d0
Apollo <102649729+Apollounknowndev@users.noreply.github.com>
2024-12-20 18:37:01 -0500

    1.3.10

--- common/src/main/java/dev/worldgen/lithostitched/worldgen/modifier/SetStructureSpawnConditionModifier.java
+++ common/src/main/java/dev/worldgen/lithostitched/worldgen/modifier/SetStructureSpawnConditionModifier.java
@@ -40,7 +40,7 @@ public record SetStructureSpawnConditionModifier(HolderSet<Structure> structures
             if (structure instanceof Holder.Reference<Structure> reference) {
                 final Structure delegating = new DelegatingStructure(new DelegatingConfig(Holder.direct(structure.value()), this.spawnCondition));
                 ((HolderReferenceAccessor<Structure>)structure).setValue(delegating);
-                ((MappedRegistryAccessor<Structure>)registries.lookupOrThrow(Registries.STRUCTURE)).getByValue().put(delegating, reference);
+                ((MappedRegistryAccessor<Structure>)registries.registryOrThrow(Registries.STRUCTURE)).getByValue().put(delegating, reference);
             }
         }
     }
