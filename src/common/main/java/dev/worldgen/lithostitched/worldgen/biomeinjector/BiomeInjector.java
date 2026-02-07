a594b2041c793b6a9cb540068ef365282eecc36c
Apollo <102649729+Apollounknowndev@users.noreply.github.com>
2026-02-06 20:42:53 -0500

    Cleaned up reserved names

--- src/common/main/java/dev/worldgen/lithostitched/worldgen/biomeinjector/BiomeInjector.java
+++ src/common/main/java/dev/worldgen/lithostitched/worldgen/biomeinjector/BiomeInjector.java
@@ -9,6 +9,7 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.util.InclusiveRange;
+import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Climate.TargetPoint;
 import net.minecraft.world.level.dimension.LevelStem;
@@ -19,41 +20,23 @@ import java.util.Map;
 import java.util.function.Function;
 
 public final class BiomeInjector {
-	public static final Codec<BiomeInjector> CODEC = RecordCodecBuilder.<BiomeInjector>create(i -> i.group(
+	public static final Codec<BiomeInjector> CODEC = RecordCodecBuilder.create(i -> i.group(
 		Biome.CODEC.fieldOf("biome").forGetter(BiomeInjector::biome),
 		ResourceKey.codec(Registries.LEVEL_STEM).fieldOf("dimension").forGetter(BiomeInjector::dimension),
 		Codec.unboundedMap(
-			Codec.either(Codec.STRING, DensityFunction.HOLDER_HELPER_CODEC),
+			Codec.either(ReservedParameter.CODEC, DensityFunction.HOLDER_HELPER_CODEC),
 			InclusiveRange.codec(Codec.DOUBLE)
 		).fieldOf("parameters").forGetter(BiomeInjector::parameters)
-	).apply(i, BiomeInjector::new)).validate(BiomeInjector::validate);
-	private static final Map<String, Function<TargetPoint, Long>> RESERVED_PARAMETERS = Map.of(
-		"continentalness", TargetPoint::continentalness,
-		"erosion", TargetPoint::erosion,
-		"weirdness", TargetPoint::weirdness,
-		"humidity", TargetPoint::humidity,
-		"temperature", TargetPoint::temperature,
-		"depth", TargetPoint::depth
-	);
+	).apply(i, BiomeInjector::new));
 	
 	private final Holder<Biome> biome;
 	private final ResourceKey<LevelStem> dimension;
-	private final Map<Either<String, DensityFunction>, InclusiveRange<Double>> parameters;
+	private final Map<Either<ReservedParameter, DensityFunction>, InclusiveRange<Double>> parameters;
 	
-	public BiomeInjector(Holder<Biome> biome, ResourceKey<LevelStem> dimension, Map<Either<String, DensityFunction>, InclusiveRange<Double>> parameters) {
+	public BiomeInjector(Holder<Biome> biome, ResourceKey<LevelStem> dimension, Map<Either<ReservedParameter, DensityFunction>, InclusiveRange<Double>> parameters) {
 		this.biome = biome;
 		this.dimension = dimension;
 		this.parameters = new HashMap<>(parameters);
-	}
-	
-	private DataResult<BiomeInjector> validate() {
-		for (var entry : this.parameters.entrySet()) {
-			var either = entry.getKey();
-			if (either.left().isPresent() && !RESERVED_PARAMETERS.containsKey(either.left().get())) {
-				return DataResult.error(() -> "Value found for non-existent parameter: " + either.left().get());
-			}
-		}
-		return DataResult.success(this);
 	}
 	
 	public void mapAll(NoiseWiringHelper noiseHelper) {
@@ -66,7 +49,7 @@ public final class BiomeInjector {
 	public boolean matches(DensityFunction.FunctionContext context, TargetPoint point) {
 		for (var entry : this.parameters.entrySet()) {
 			double density = entry.getKey().map(
-				string -> RESERVED_PARAMETERS.get(string).apply(point) / 10000D,
+				reserved -> reserved.getter.apply(point) / 10000D,
 				df -> df.compute(context)
 			);
 			if (!entry.getValue().isValueInRange(density)) return false;
@@ -82,7 +65,31 @@ public final class BiomeInjector {
 		return dimension;
 	}
 	
-	private Map<Either<String, DensityFunction>, InclusiveRange<Double>> parameters() {
+	private Map<Either<ReservedParameter, DensityFunction>, InclusiveRange<Double>> parameters() {
 		return parameters;
+	}
+	
+	public enum ReservedParameter implements StringRepresentable {
+		CONTINENTALNESS("continentalness", TargetPoint::continentalness),
+		EROSION("erosion", TargetPoint::erosion),
+		WEIRDNESS("weirdness", TargetPoint::weirdness),
+		HUMIDITY("humidity", TargetPoint::humidity),
+		TEMPERATURE("temperature", TargetPoint::temperature),
+		DEPTH("depth", TargetPoint::depth);
+		
+		public static final Codec<ReservedParameter> CODEC = StringRepresentable.fromEnum(ReservedParameter::values);
+		
+		public final String name;
+		public final Function<TargetPoint, Long> getter;
+		
+		ReservedParameter(String name, Function<TargetPoint, Long> getter) {
+			this.name = name;
+			this.getter = getter;
+		}
+		
+		@Override
+		public String getSerializedName() {
+			return this.name;
+		}
 	}
 }
