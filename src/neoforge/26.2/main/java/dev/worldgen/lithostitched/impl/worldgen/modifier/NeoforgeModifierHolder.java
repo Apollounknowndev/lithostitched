e70656e0e4f6610205beb6cb37e6f1e518671fd2
Apollo <102649729+Apollounknowndev@users.noreply.github.com>
2026-08-07 22:13:36 -0400

    Remove other version code

--- src/neoforge/26.2/main/java/dev/worldgen/lithostitched/impl/worldgen/modifier/NeoforgeModifierHolder.java
+++
@@ -1,7 +0,0 @@
-package dev.worldgen.lithostitched.impl.worldgen.modifier;
-
-import net.neoforged.neoforge.common.world.BiomeModifier;
-
-public interface NeoforgeModifierHolder {
-	BiomeModifier createNeoforgeModifier();
-}
